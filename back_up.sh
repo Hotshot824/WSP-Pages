b7516ea5778bcb67de8fc2192bd1c1602eafb4d8
Benson <39863539+Hotshot824@users.noreply.github.com>
2023-12-22 23:21:05 +0800

    Update back_up.sh

--- back_up.sh
+++ back_up.sh
@@ -3,5 +3,5 @@ WD=$(dirname $(readlink -f $0));
 cTime=$(date +"%d_%m_%y");
 cd ${WD};
 sudo tar zcvf '../WSP_'${cTime}'.tar.gz' '../WSP';
-scp '../WSP_'${cTime}'.tar.gz' user@140.127.196.150:~/WSP-file/;
+scp '../WSP_'${cTime}'.tar.gz' ${ backupServer };
 rm -rf '../WSP_'${cTime}'.tar.gz';
